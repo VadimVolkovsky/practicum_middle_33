c70189d19a0709774baab74e4cc41242fd4a4044
Yurii Kolpakov <yurii.kolpakov@newstream.ai>
2024-05-08 00:47:18 +0300

    move search

--- src/api/v1/films.py
+++ src/api/v1/films.py
@@ -23,7 +23,7 @@ class FilmListSerializer(BaseModel):
     imdb_rating: float
 
 
-class FilmSerializer(BaseModel):
+class FilmSerializer(FilmListSerializer):
     description: str
     genre: list[dict[str, str]]
     directors: list[dict[str, str]]
@@ -31,6 +31,23 @@ class FilmSerializer(BaseModel):
     writers: list[dict[str, str]]
     file_path: Optional[str]
     creation_date: Optional[datetime]
+
+
+@router.get('/search', response_model=list[FilmListSerializer])
+async def film_search(query: str,
+                      page_number: int = Query(1, gt=0),
+                      page_size: int = Query(100, gt=0),
+                      sort: Optional[str] = None,
+                      film_service: FilmService = Depends(get_film_service)) -> FilmListSerializer:
+    start_index = (page_number - 1) * page_size
+
+    film_list = await film_service.get_list_film(start_index, page_size, sort=sort, query=query)
+
+    if not film_list:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='films not found')
+
+    # return FilmListSerializer(*film_list)
+    return film_list
 
 
 @router.get('/{film_id}', response_model=FilmSerializer)
@@ -62,18 +79,20 @@ async def film_list(page_number: int = Query(1, gt=0),
     return film_list
 
 
-@router.get('/search', response_model=list[FilmListSerializer])
-async def film_search(query: str,
-                      page_number: int = Query(1, gt=0),
-                      page_size: int = Query(100, gt=0),
-                      sort: Optional[str] = None,
-                      film_service: FilmService = Depends(get_film_service)) -> list[FilmListSerializer]:
-    start_index = (page_number - 1) * page_size
+# @router.get('/search', response_model=list[FilmListSerializer])
+# async def search(query: str,
+#                  page_number: int = Query(1, gt=0),
+#                  page_size: int = Query(100, gt=0),
+#                  sort: Optional[str] = None,
+#                  film_service: FilmService = Depends(get_film_service)) -> list[FilmListSerializer]:
+#     start_index = (page_number - 1) * page_size
+#
+#     film_list = await film_service.get_list_film(start_index, page_size, sort=sort, query=query)
+#
+#     if not film_list:
+#         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='films not found')
+#
+#     # return FilmListSerializer(*film_list)
+#     return film_list
 
-    film_list = await film_service.get_list_film(start_index, page_size, sort=sort, query=query)
 
-    if not film_list:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='films not found')
-
-    # return FilmListSerializer(*film_list)
-    return film_list
