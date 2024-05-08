9e64de73efb45146aa08f0a0104682a98cbb0801
Yurii Kolpakov <yurii.kolpakov@newstream.ai>
2024-05-08 12:30:49 +0300

    add comments to query_dict

--- src/services/utils.py
+++ src/services/utils.py
@@ -6,6 +6,12 @@ async def _get_query_body(start_index: int,
                           sort: Optional[str] = None,
                           genre: Optional[str] = None,
                           query: Optional[str] = None) -> dict:
+    '''функция для составления запроса поиска в elassticsearch
+    :param start_index: номер записи с которой начинается выдача записей с ES
+    :param page_size: размер станицы
+    :param genre: жанр, по которому фильтруется список фильмов
+    :param sort: поле, по которому ссортируется список'''
+
     body = {
         'size': page_size,
         'from': start_index,
