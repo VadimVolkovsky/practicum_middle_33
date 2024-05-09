103cc10d445bf6e5f9c3bf2bb57c463375acea63
VadimVolkovsky <vadimas29@gmail.com>
2024-05-09 10:27:23 +0300

    Добавлен эндпоинт "Страница персонажа - Данные по персоне."

--- src/api/v1/endpoints/persons.py
+++ src/api/v1/endpoints/persons.py
@@ -1,8 +1,30 @@
-from datetime import datetime
 from http import HTTPStatus
 from typing import Optional
 
-from fastapi import APIRouter, Depends, HTTPException, Query
-from pydantic import BaseModel
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel, Field
+
+from services.person import PersonService, get_person_service, PersonsFilms
 
 router = APIRouter()
+
+
+class PersonSerializer(BaseModel):
+    id: str
+    name: str = Field(..., alias='full_name')
+    films: Optional[list[PersonsFilms]]
+
+    class Config:
+        allow_population_by_field_name = True
+
+
+@router.get('/{person_id}', response_model=PersonSerializer)
+async def person_detail(person_id: str,
+                        person_service: PersonService = Depends(get_person_service)) -> PersonSerializer:
+    """Получение информации о персонаже, со списком его фильмов и ролей"""
+    person_with_films = await person_service.get_by_id(person_id)
+
+    if not person_with_films:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='person not found')
+
+    return PersonSerializer(**person_with_films)
--- src/services/utils.py
+++ src/services/utils.py
@@ -7,11 +7,13 @@ async def _get_query_body(start_index: int,
                           page_size: int,
                           sort: Optional[str] = None,
                           genre: Optional[str] = None,
+                          person_id: Optional[str] = None,
                           query: Optional[str] = None) -> dict:
     '''функция для составления запроса поиска в elassticsearch
     :param start_index: номер записи с которой начинается выдача записей с ES
     :param page_size: размер станицы
     :param genre: жанр, по которому фильтруется список фильмов
+    :param person_id: айди персонажа, по которому фильтруется список фильмов
     :param sort: поле, по которому ссортируется список'''
 
     body = {
@@ -38,6 +40,27 @@ async def _get_query_body(start_index: int,
                 }
             },
             'inner_hits': {
+            }
+        }
+
+    if person_id:
+        if not body.get('query', None):
+            body['query'] = {}
+
+        body = {
+            "query": {
+                "nested": {
+                    "path": "writers",
+                    "query": {
+                        "bool": {
+                            "should": [
+                                {"match": {"directors.id": person_id}},
+                                {"match": {"actors.id": person_id}},
+                                {"match": {"writers.id": person_id}},
+                            ]
+                        }
+                    }
+                }
             }
         }
 
