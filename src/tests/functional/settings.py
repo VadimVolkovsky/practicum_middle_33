e0b7c414ad663d50f7843927802485f440707a79
VadimVolkovsky <vadimas29@gmail.com>
2024-05-20 18:10:41 +0300

    Апдейт процесса дебага тестов

--- src/tests/functional/settings.py
+++ src/tests/functional/settings.py
@@ -3,12 +3,13 @@ from pydantic import Field
 
 
 class TestSettings(BaseSettings):
+    # Настройки для запуска тестирования в контейнерах
     es_host: str = Field('elasticsearch:9200', env='ELASTIC_HOST')
     service_url: str = 'http://api:8000'
     redis_host: str = 'redis'
     redis_port: int = 6379
 
-    # для локального дебага тестов
+    # # Настройки для локального дебага тестов
     # es_host: str = Field('127.0.0.1:9200', env='ELASTIC_HOST')
     # service_url: str = 'http://127.0.0.1:8000'
     # redis_host: str = '127.0.0.1'
