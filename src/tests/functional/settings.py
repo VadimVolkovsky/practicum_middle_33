092ced444bdc9799bbb3a8868e55d3c5ba7e3aed
Yurii Kolpakov <yurii.kolpakov@newstream.ai>
2024-05-20 20:34:19 +0300

    tests

--- src/tests/functional/settings.py
+++ src/tests/functional/settings.py
@@ -3,10 +3,9 @@ from pydantic import Field
 
 
 class TestSettings(BaseSettings):
-    elastic_host: str = Field('127.0.0.1:9200')
-    # es_port: int = Field(9200, env='ELASTIC_PORT')
+    elastic_host: str = Field('elasticsearch:9200', env='ELASTIC_HOST')
     service_url: str = 'http://api:8000'
-    redis_host: str = Field('127.0.0.1')
+    redis_host: str = 'redis'
     redis_port: int = 6379
 
     # для локального дебага тестов
