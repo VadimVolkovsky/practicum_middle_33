d222e859e5a6385b5ab641ebc2863667828a0f69
Yurii Kolpakov <yurii.kolpakov@newstream.ai>
2024-05-21 19:42:53 +0300

    file with test data

---
+++ src/tests/functional/testdata/es_test_data.py
@@ -0,0 +1,593 @@
+es_data = [
+    {
+        "id": "5ffeb144-fbf1-4658-88b3-2355465778ed",
+        "imdb_rating": 9.1,
+        "genre": [
+            {
+                "id": "6f476c58-40e3-48a1-a10f-0f23e368fb66",
+                "name": "Detective"
+            },
+            {
+                "id": "3ef47d13-600e-4c19-b576-6d7eae12e7fa",
+                "name": "Comedy"
+            }
+        ],
+        "title": "Repurpose Proactive Architectures",
+        "description": "Personal toward race ready. Have least outside feeling nothing push money ability. "
+                       "Imagine friend far shake actually themselves natural. Argue person make share theory.",
+        "directors": [
+            {
+                "id": "e68c1e30-a0a0-4d1f-a684-c8885b48b90d",
+                "name": "Billy Montes"
+            },
+            {
+                "id": "6c4d7e19-0f1d-4172-b2f1-ebc386a4373e",
+                "name": "Danielle Hamilton"
+            },
+            {
+                "id": "854b37ae-ade8-4be1-ba73-8381381d3297",
+                "name": "Alex Brown"
+            }
+        ],
+        "actors": [
+            {
+                "id": "7a54e97b-8e5c-4d04-aa56-a881d6a1ac4b",
+                "name": "Christopher Herring"
+            }
+        ],
+        "writers": [
+            {
+                "id": "cdbe0c09-4649-4c61-9f0b-64acf723fbc8",
+                "name": "Ashley Foster MD"
+            },
+            {
+                "id": "437a60df-5052-4a19-953c-eec97debc675",
+                "name": "Tristan Lopez"
+            }
+        ]
+    },
+    {
+        "id": "77fd1075-46bc-4695-913a-b5ae2f9f4239",
+        "imdb_rating": 1.3,
+        "genre": [
+            {
+                "id": "06fa5fc5-a84a-499f-826a-2c79e49b50a2",
+                "name": "Western"
+            },
+            {
+                "id": "6f476c58-40e3-48a1-a10f-0f23e368fb66",
+                "name": "Detective"
+            }
+        ],
+        "title": "Expedite Clicks-And-Mortar Markets",
+        "description": "Sing general relationship recently him lot enter. Wrong piece accept TV.",
+        "directors": [
+            {
+                "id": "10fa7c4d-0c20-4e85-a8ec-6833b62feb3a",
+                "name": "Mr. Devin Dickson"
+            }
+        ],
+        "actors": [
+            {
+                "id": "4d92de5d-b6c9-4008-95e6-1d6caf6f7782",
+                "name": "Katherine Black"
+            },
+            {
+                "id": "0adbcb04-2674-464b-99b6-229a34358d5f",
+                "name": "Matthew Rice"
+            },
+            {
+                "id": "b053d005-6cd8-45b3-8f5f-8bbae6acdcd7",
+                "name": "Robert Miller"
+            }
+        ],
+        "writers": [
+            {
+                "id": "516f734f-bd7e-424c-b328-bbfeba593db7",
+                "name": "Michael Reeves"
+            },
+            {
+                "id": "73061dfa-ea93-45ff-b6e2-0b771f81cef3",
+                "name": "Laura Brooks"
+            },
+            {
+                "id": "8b2f46dd-b0ce-4a47-b77a-894d9920550b",
+                "name": "Stacey Moran"
+            },
+            {
+                "id": "7c7d3c8e-9499-49ec-b75b-a8e17c52fe1b",
+                "name": "Cheryl Smith"
+            },
+            {
+                "id": "c75b6339-e25b-444b-92fc-cadbb5ed43ca",
+                "name": "Tamara Prince"
+            }
+        ]
+    },
+    {
+        "id": "3ee69719-dca0-4bc0-8cba-ad0d77fdf52d",
+        "imdb_rating": 7.4,
+        "genre": [
+            {
+                "id": "06fa5fc5-a84a-499f-826a-2c79e49b50a2",
+                "name": "Western"
+            },
+            {
+                "id": "6f476c58-40e3-48a1-a10f-0f23e368fb66",
+                "name": "Detective"
+            },
+            {
+                "id": "9e5b5398-dcef-4d70-8dba-4010750cfe9d",
+                "name": "Drama"
+            }
+        ],
+        "title": "Unleash B2C Methodologies",
+        "description": "Number final outside. Politics happen painting early.\nSize follow same into. Participant him "
+                       "heart science give push art.\nWhole water major. Support Mr argue also reach.",
+        "directors": [
+            {
+                "id": "91d5eddb-1659-4c78-8d7b-f2467219af9b",
+                "name": "Dustin Hill"
+            },
+            {
+                "id": "a59c6b37-084a-41ff-ba80-3934453a1620",
+                "name": "Gina King"
+            }
+        ],
+        "actors": [
+            {
+                "id": "4d92de5d-b6c9-4008-95e6-1d6caf6f7782",
+                "name": "Katherine Black"
+            },
+            {
+                "id": "953a505e-034b-4474-82b1-595f7d4332b0",
+                "name": "Shaun Thompson"
+            },
+            {
+                "id": "48a656dc-61ff-4a1a-9288-c15cd6c513a0",
+                "name": "Javier Chang"
+            }
+        ],
+        "writers": [
+            {
+                "id": "ca8ca566-622a-49a9-94ef-73984ebdf4ac",
+                "name": "Justin Huber"
+            },
+            {
+                "id": "751c4d59-527f-4583-8210-1429044609de",
+                "name": "Nicole Green"
+            },
+            {
+                "id": "8e41fe3b-c970-45d7-a62f-c1af05a38951",
+                "name": "Kelly Nguyen"
+            },
+            {
+                "id": "8e7c52ec-9eb5-4f17-b957-d45b43b42919",
+                "name": "Adam Galvan"
+            },
+            {
+                "id": "4d6ef771-5da7-4aa7-a246-aebfb90a3c18",
+                "name": "Vincent Maynard"
+            }
+        ]
+    },
+    {
+            "id": "a3621c6c-f357-4cc8-8746-af33d7593316",
+            "imdb_rating": 3.5,
+            "genre": [
+                {
+                    "id": "4bfe0361-b6bd-4268-90f9-a8b1b81ff875",
+                    "name": "Melodrama"
+                },
+                {
+                    "id": "06fa5fc5-a84a-499f-826a-2c79e49b50a2",
+                    "name": "Western"
+                },
+                {
+                    "id": "6f476c58-40e3-48a1-a10f-0f23e368fb66",
+                    "name": "Detective"
+                }
+            ],
+            "title": "Transition Viral Methodologies",
+            "description": "How skill husband work. Explain method hear such. Loss cover behavior market.\nLittle "
+                           "couple green process. Answer can population special inside Democrat personal. Require "
+                           "majority myself not.",
+            "directors": [
+                {
+                    "id": "0043302c-73a9-489b-b002-13fdcdcfc428",
+                    "name": "Megan Lopez"
+                },
+                {
+                    "id": "a6458879-affd-46f5-9113-7e038283ce68",
+                    "name": "Spencer Wilson"
+                },
+                {
+                    "id": "52b5210b-33e0-47b2-a3db-12894d010dd1",
+                    "name": "Dr. Carol Graham MD"
+                },
+                {
+                    "id": "46f95f64-6f94-4899-a23e-0bc99fb59907",
+                    "name": "Terrence Owens"
+                },
+                {
+                    "id": "55fcf6a2-b402-4555-a5b9-e86e964c8472",
+                    "name": "Natasha Mcgrath"
+                }
+            ],
+            "actors": [
+                {
+                    "id": "840c9ccc-4d2d-4472-a003-c625af378d74",
+                    "name": "Jennifer Moore"
+                },
+                {
+                    "id": "9d478380-1d26-4e30-b5a6-f98bb025b315",
+                    "name": "Jared Price"
+                },
+                {
+                    "id": "4120e391-dd80-45f0-92f8-29a5306a99b0",
+                    "name": "Taylor Turner"
+                }
+            ],
+            "writers": [
+                {
+                    "id": "4fdbb5b6-c8ed-4f96-9cd5-939d2dafda9a",
+                    "name": "Cynthia Brown"
+                },
+                {
+                    "id": "84e8ae23-7f54-4351-a7ea-fe28ecb8eac7",
+                    "name": "Jeffrey Lowery"
+                },
+                {
+                    "id": "ed4403cf-28bd-4471-822b-7b2d6d88c084",
+                    "name": "Christina Kim"
+                }
+            ]
+        },
+    {
+        "id": "a606fc9e-9192-4960-9e9f-863f0e20ec88",
+        "imdb_rating": 5.4,
+        "genre": [
+            {
+                "id": "9e5b5398-dcef-4d70-8dba-4010750cfe9d",
+                "name": "Drama"
+            },
+            {
+                "id": "6f476c58-40e3-48a1-a10f-0f23e368fb66",
+                "name": "Detective"
+            }
+        ],
+        "title": "Strategize Plug-And-Play Markets",
+        "description": "This because gun peace spend west soldier. Itself yes decide somebody understand student "
+                       "direction. List know available pretty season risk move.\nNumber may thing myself back "
+                       "black.",
+        "directors": [
+            {
+                "id": "57136fec-ed8f-4da8-b509-7fd83672cc92",
+                "name": "Michael Martin"
+            },
+            {
+                "id": "6222775c-ad08-4c3d-9e7c-532e6de232ab",
+                "name": "Charles Long"
+            }
+        ],
+        "actors": [
+            {
+                "id": "497bfda1-a2a9-476f-b1c5-1c5ddffc411d",
+                "name": "Charles Avila"
+            },
+            {
+                "id": "9d478380-1d26-4e30-b5a6-f98bb025b315",
+                "name": "Jared Price"
+            },
+            {
+                "id": "3e09fb42-7506-4b86-a12a-381a404482c9",
+                "name": "Alicia Glover"
+            }
+        ],
+        "writers": [
+            {
+                "id": "a392c86e-a433-46fe-a1ad-307d1fb1327e",
+                "name": "Michael Payne"
+            }
+        ]
+    },
+    {
+        "id": "ea718828-a2c7-4555-bcc9-828e83cd4e56",
+        "imdb_rating": 9.4,
+        "genre": [
+            {
+                "id": "4bfe0361-b6bd-4268-90f9-a8b1b81ff875",
+                "name": "Melodrama"
+            },
+            {
+                "id": "6f476c58-40e3-48a1-a10f-0f23e368fb66",
+                "name": "Detective"
+            },
+            {
+                "id": "06fa5fc5-a84a-499f-826a-2c79e49b50a2",
+                "name": "Western"
+            }
+        ],
+        "title": "Synthesize Virtual E-Business",
+        "description": "Task business thing manage play. Example plan describe whom involve ago traditional "
+                       "direction. Reflect produce take loss hair win.",
+        "directors": [
+            {
+                "id": "fcb45c4b-5fc7-4e47-a8ad-49a71d6f5f38",
+                "name": "Valerie Thompson"
+            }
+        ],
+        "actors": [
+            {
+                "id": "8f380dcb-c10d-42e7-8137-ffe85c30cdc4",
+                "name": "Mr. Andrew Stevens"
+            },
+            {
+                "id": "4120e391-dd80-45f0-92f8-29a5306a99b0",
+                "name": "Taylor Turner"
+            },
+            {
+                "id": "1d4c8bbe-b2a4-4bf9-8780-c5fb33e0be82",
+                "name": "Richard Casey"
+            },
+            {
+                "id": "a08e59bc-28bb-41ee-967c-5852a020d462",
+                "name": "Matthew Fleming"
+            },
+            {
+                "id": "1e7e8a0e-8b61-48bb-bc1c-923b2228cb79",
+                "name": "Daniel Welch"
+            },
+            {
+                "id": "cf2b9154-1e75-46c6-990f-ac6edaf980a0",
+                "name": "Melissa Williams"
+            }
+        ],
+        "writers": [
+            {
+                "id": "0fd52e0d-5357-4014-a113-d8d6ae6feb1c",
+                "name": "John Barker MD"
+            },
+            {
+                "id": "e0c2ae32-bd15-414a-9ed0-43ae0ac6079b",
+                "name": "Jonathan Benton"
+            },
+            {
+                "id": "1f33b930-8084-4e65-8cd6-bb8b3eb5ce84",
+                "name": "Hannah Diaz"
+            }
+        ]
+    },
+    {
+        "id": "a4805438-81a8-42c1-815b-11813f1e2e9f",
+        "imdb_rating": 9.5,
+        "genre": [
+            {
+                "id": "06fa5fc5-a84a-499f-826a-2c79e49b50a2",
+                "name": "Western"
+            }
+        ],
+        "title": "Iterate B2C Web Services",
+        "description": "Wind machine unit wait. Individual president particular although Congress. Significant "
+                       "skin so wear.",
+        "directors": [],
+        "actors": [
+            {
+                "id": "81cf5af6-88e9-4987-a2a3-66e2481c742d",
+                "name": "Kristine Sawyer"
+            },
+            {
+                "id": "87d77a80-2a0f-468e-8cd6-ef0680057191",
+                "name": "Jeremy Harris"
+            },
+            {
+                "id": "826498a1-f1ee-4465-a9bc-cee01aea83eb",
+                "name": "Jacqueline Suarez"
+            }
+        ],
+        "writers": [
+            {
+                "id": "d5fc46b6-44e1-48ef-a1b2-86bfaec49c9e",
+                "name": "Aaron Jackson"
+            },
+            {
+                "id": "8e7c52ec-9eb5-4f17-b957-d45b43b42919",
+                "name": "Adam Galvan"
+            },
+            {
+                "id": "100d7244-dfd4-40ea-978c-e58ff1fabd2f",
+                "name": "Penny Jackson"
+            },
+            {
+                "id": "d15c92fa-4908-448f-94c3-743e4060db81",
+                "name": "Rhonda Johnson"
+            },
+            {
+                "id": "ba5ef465-ca73-4118-83b6-816af6c52ed5",
+                "name": "Angel Jefferson"
+            },
+            {
+                "id": "09697f17-1826-4b46-80ea-88dae4105746",
+                "name": "Dr. Misty Swanson"
+            },
+            {
+                "id": "49ae5319-177f-4051-ace4-0c677fa6eb44",
+                "name": "Brett Pena"
+            },
+            {
+                "id": "4b94a613-9e41-4dc6-bf57-79339a5dcac2",
+                "name": "Justin Combs"
+            }
+        ]
+    },
+    {
+        "id": "def3d3a5-bb6a-48b5-98e2-4de6e943d4ed",
+        "imdb_rating": 9.2,
+        "genre": [
+            {
+                "id": "9e5b5398-dcef-4d70-8dba-4010750cfe9d",
+                "name": "Drama"
+            }
+        ],
+        "title": "Enable 24/365 Web-Readiness",
+        "description": "Last change story increase today idea sell degree. Between factor road. Watch weight "
+                       "specific call reveal person option.",
+        "directors": [
+            {
+                "id": "3f7ce42e-6dd1-4287-856f-74fcbaf67d9d",
+                "name": "Jamie Brown"
+            },
+            {
+                "id": "91d5eddb-1659-4c78-8d7b-f2467219af9b",
+                "name": "Dustin Hill"
+            }
+        ],
+        "actors": [
+            {
+                "id": "24a4ba7d-896a-4f03-a193-7f8dfec79904",
+                "name": "Roberto Campbell"
+            },
+            {
+                "id": "b8516419-fc3b-4500-b340-3453061a2e1b",
+                "name": "Jason Barnes"
+            },
+            {
+                "id": "7dabeafa-e444-47d3-99c5-61df90e41890",
+                "name": "Jennifer Morgan"
+            },
+            {
+                "id": "04d9bc0b-1c5e-4dbf-b7ea-7f95c4c8b55e",
+                "name": "Stacey Ortega"
+            }
+        ],
+        "writers": [
+            {
+                "id": "8e7c52ec-9eb5-4f17-b957-d45b43b42919",
+                "name": "Adam Galvan"
+            }
+        ]
+    },
+    {
+        "id": "06814c21-8be0-4a4a-8311-3a0dbe5826ce",
+        "imdb_rating": 2.2,
+        "genre": [
+            {
+                "id": "06fa5fc5-a84a-499f-826a-2c79e49b50a2",
+                "name": "Western"
+            },
+            {
+                "id": "4bfe0361-b6bd-4268-90f9-a8b1b81ff875",
+                "name": "Melodrama"
+            },
+            {
+                "id": "6f476c58-40e3-48a1-a10f-0f23e368fb66",
+                "name": "Detective"
+            }
+        ],
+        "title": "Brand E-Business Action-Items",
+        "description": "Inside soon government lot source door tend she. Relationship by onto for. Arrive "
+                       "generation their skill degree. Clear either drive international first cost world.",
+        "directors": [
+            {
+                "id": "0de0b536-acfb-4a66-9f50-d220250a0cb7",
+                "name": "Kenneth Martinez"
+            },
+            {
+                "id": "613d9060-8510-4f79-864c-fb2bc5a44c20",
+                "name": "Stephen Thompson"
+            }
+        ],
+        "actors": [
+            {
+                "id": "826498a1-f1ee-4465-a9bc-cee01aea83eb",
+                "name": "Jacqueline Suarez"
+            },
+            {
+                "id": "04d9bc0b-1c5e-4dbf-b7ea-7f95c4c8b55e",
+                "name": "Stacey Ortega"
+            },
+            {
+                "id": "609e9688-cdbb-4ec5-af34-eb44f59fa3a0",
+                "name": "Deborah Cooper"
+            }
+        ],
+        "writers": [
+            {
+                "id": "d26fb53d-9571-4700-843e-c96fc87d4991",
+                "name": "Jamie Hill"
+            },
+            {
+                "id": "ed4403cf-28bd-4471-822b-7b2d6d88c084",
+                "name": "Christina Kim"
+            },
+            {
+                "id": "970fc53d-4b1a-4890-8968-e007285022b8",
+                "name": "Teresa Green"
+            },
+            {
+                "id": "516f734f-bd7e-424c-b328-bbfeba593db7",
+                "name": "Michael Reeves"
+            },
+            {
+                "id": "62bf66fa-1a42-424b-a546-60c89f16fb51",
+                "name": "Steven Contreras"
+            }
+        ]
+        },
+    {
+        "id": "50c1a06b-c11b-439d-9140-e7ec67d2c90f",
+        "imdb_rating": 1.6,
+        "genre": [
+            {
+                "id": "4bfe0361-b6bd-4268-90f9-a8b1b81ff875",
+                "name": "Melodrama"
+            },
+            {
+                "id": "06fa5fc5-a84a-499f-826a-2c79e49b50a2",
+                "name": "Western"
+            },
+            {
+                "id": "6f476c58-40e3-48a1-a10f-0f23e368fb66",
+                "name": "Detective"
+            }
+        ],
+        "title": "Re-Contextualize Wireless Platforms",
+        "description": "Wrong drive interest toward since movie scene. Thank foreign note reason two option "
+                       "learn. Health simple economy difference time condition.\nThus human film herself give. "
+                       "Number out close.",
+        "directors": [
+            {
+                "id": "256a06a2-3899-4cf2-b116-35fe4685e4e6",
+                "name": "Jason Pearson"
+            },
+            {
+                "id": "193960ad-7280-405f-9b83-c080e95209d6",
+                "name": "Jennifer Lopez"
+            }
+        ],
+        "actors": [
+            {
+                "id": "92f4cec7-3fa5-4405-b1fa-37fcd0e44bdd",
+                "name": "Beth Peterson"
+            },
+            {
+                "id": "bc62387e-dbcd-481e-a93e-9d0409820e32",
+                "name": "Eric Francis"
+            }
+        ],
+        "writers": [
+            {
+                "id": "7ef8ba46-0ccc-4514-90ec-a9a270245c02",
+                "name": "Jason Hernandez"
+            },
+            {
+                "id": "bc289f74-11f3-4b2d-8d9b-6519c2f3eee9",
+                "name": "Teresa Sloan"
+            },
+            {
+                "id": "d09809d6-db75-46e2-8344-16267709836b",
+                "name": "Melinda Bowers"
+            }
+        ]
+    },
+]
