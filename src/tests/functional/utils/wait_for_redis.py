dcf90f7495ff2592bdcd25747698e0e8af0d03aa
VadimVolkovsky <vadimas29@gmail.com>
2024-05-23 18:50:19 +0300

    Бэкофф для wait_for_es и wait_for_redis

--- src/tests/functional/utils/wait_for_redis.py
+++ src/tests/functional/utils/wait_for_redis.py
@@ -1,13 +1,24 @@
-import time
+import logging
+import os
 
 from redis import Redis
 
 from tests.functional.settings import test_settings
 
+logger = logging.getLogger(os.path.basename(__file__))
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+
+def main():
+    redis = Redis(host=test_settings.redis_host, port=test_settings.redis_port)
+    if redis.ping():
+        logger.info("Redis connection OK")
+        return
+    else:
+        logger.info("Waiting for connection to Redis...")
+        raise ConnectionError
+
 
 if __name__ == '__main__':
-    redis = Redis(host=test_settings.redis_host, port=test_settings.redis_port)
-    while True:
-        if redis.ping():
-            break
-        time.sleep(1)
+    main()
